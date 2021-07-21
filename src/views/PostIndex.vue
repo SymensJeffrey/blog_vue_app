a1deb096afafd421548d5ad39a90bbd2a2327a3d
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-21 18:34:30 -0500

    init

---
+++ src/views/PostIndex.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1> Posts </h1>
+    <div v-for="post in posts">
+      <p> {{ post.id }} </p>
+      <h3> {{ post.title }} </h3>
+      <p> {{ post.body }} </p>
+      <p> <img v-bind:src="post.image"> </p>
+      <p> ---------------------------------------------------------------- </p>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        posts: [],
+      };
+    },
+    created: function () {
+      this.indexPosts ();
+    },
+    methods: {
+      indexPosts: function () {
+          console.log("indexing...")
+          axios.get("http://localhost:3000/posts").then(response => {
+          console.log(response.data);
+          this.posts = response.data;
+        })
+      },
+    }
+  };
+</script>
+
---
+++ src/views/PostNew.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <h1> Create new Post </h1>
+    <div>
+      <p>Title: <input type="text" v-model="newPostParams.title"></p>
+      <p>Post: <input type="text" v-model="newPostParams.body"></p>
+      <p>Image: <input type="text" v-model="newPostParams.image"></p>
+      <button v-on:click="createPost()">Post</button>
+
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        newPostParams: {},
+      };
+    },
+    created: function () {
+    },
+    methods: {
+      createPost: function () {
+        console.log("creating....")
+        axios
+          .post("/posts", this.newPostParams)
+          .then((response) => {
+            console.log(response.data);
+            this.$router.push("/posts");
+          })
+          .catch((error) => {
+            this.errors = error.response.data.errors;
+          });
+      },
+    },
+  };
+</script>
+
