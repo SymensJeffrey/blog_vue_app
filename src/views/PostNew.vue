0d9be043d3e586c1fdd344df74e7db7b01dcd8c9
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-21 19:51:15 -0500

    make input box bigger for new post body

--- src/views/PostNew.vue
+++ src/views/PostNew.vue
@@ -3,7 +3,7 @@
     <h1> Create new Post </h1>
     <div>
       <p>Title: <input type="text" v-model="newPostParams.title"></p>
-      <p>Post: <input type="text" v-model="newPostParams.body"></p>
+      <p>Post: <input class="box" type="text" v-model="newPostParams.body"></p>
       <p>Image: <input type="text" v-model="newPostParams.image"></p>
       <button v-on:click="createPost()">Post</button>
 
@@ -11,7 +11,10 @@
   </div>
 </template>
 
-<style></style>
+<style>
+.box { 
+  width: 350px; height: 100px; border: 1px solid #999999; padding: 5px; 
+  }</style>
 
 <script>
   import axios from "axios";
